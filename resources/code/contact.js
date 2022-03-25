9ec42f3bba0d026b8ef332435e0001b3e59932f5
ZyraClaudius <100091466+ZyraClaudius@users.noreply.github.com>
2022-03-25 12:41:52 +0000

    Complete styles and javascript

---
+++ resources/code/contact.js
@@ -0,0 +1,98 @@
+/* Platform Info Displays*/
+let infos = document.getElementsByClassName('platform-info');
+for(let info of infos){
+    info.setAttribute('data-toggled','');
+    info.setAttribute('data-fixed','')
+}
+
+function toggleFix(event){
+    let respInfo = document.getElementById(event.target.id+'info');
+    if(respInfo.getAttribute('data-fixed')){
+        respInfo.setAttribute('data-fixed','')
+    } else{
+        respInfo.setAttribute('data-fixed','true')
+    }
+}
+
+function toggleDisplay(event){
+    console.log(event.target);
+    let target = event.target.id;
+    console.log(target);
+    let respInfoId = target+'info';
+    console.log('respInfoId: '+respInfoId);
+    let respInfo = document.getElementById(respInfoId);
+    if(respInfo.getAttribute('data-toggled')){
+        if(!respInfo.getAttribute('data-fixed')){
+            respInfo.style.display = 'none';
+        }
+        respInfo.setAttribute('data-toggled','');
+        respInfo.setAttribute('data-fixed','');
+    } else{
+        document.getElementById('instagraminfo').style.display = 'none';
+        document.getElementById('emailinfo').style.display = 'none';
+        respInfo.style.display = 'flex';
+        respInfo.setAttribute('data-toggled','true');
+    }
+}
+let bobbles = document.querySelectorAll('.platform img');
+for(let bobble of bobbles){
+    bobble.addEventListener('mouseover',toggleDisplay);
+    bobble.addEventListener('mouseout', toggleDisplay);
+}
+
+document.getElementById('instagram').addEventListener('click',toggleFix);
+document.getElementById('email').addEventListener('click',toggleFix);
+
+/* Copy Email */
+function copyEmail(){
+    navigator.clipboard.writeText('zyraclaudius@hotmail.com');
+}
+document.getElementById('copy-email').addEventListener('click',copyEmail);
+
+/* Secret Button */
+let switched = false;
+function switchColors() {
+    console.log('Wow! You found the secret button:)');
+    if(switched){
+        switched = false;
+        document.body.style.backgroundColor = 'white';
+        for(let element of document.getElementsByTagName('*')){
+            element.style.color = 'black';
+        }
+        document.querySelector('nav').style.backgroundColor = 'white';
+        let bobbles = document.querySelectorAll('.bobble');
+        for(let bobble of bobbles){
+            bobble.style.borderColor = 'black';
+            bobble.style.backgroundColor = 'white';
+        }
+        let platformInfos = document.querySelectorAll('.platform-info');
+        for(let platformInfo of platformInfos){
+            platformInfo.style.borderColor = 'black';
+        }
+        let platformInfoButton = document.querySelector('.platform-info button');
+        platformInfoButton.style.color = 'white';
+        platformInfoButton.style.backgroundColor = 'black';
+    } else{
+        switched = true;
+        document.body.style.backgroundColor = 'black';
+        for(let element of document.getElementsByTagName('*')){
+            element.style.color = 'white';
+        }
+        document.querySelector('nav').style.backgroundColor = 'black';
+        let bobbles = document.querySelectorAll('.bobble');
+        for(let bobble of bobbles){
+            bobble.style.borderColor = 'white';
+            bobble.style.backgroundColor = 'white';
+        }
+        let platformInfos = document.querySelectorAll('.platform-info');
+        for(let platformInfo of platformInfos){
+            platformInfo.style.borderColor = 'white';
+        }
+        let platformInfoButton = document.querySelector('.platform-info button');
+        platformInfoButton.style.color = 'black';
+        platformInfoButton.style.backgroundColor = 'white';
+    }
+}
+
+let secretButton = document.getElementById('secretbutton');
+secretButton.addEventListener('click',switchColors);
--- resources/code/index.js
+++ resources/code/index.js
@@ -1,7 +1,55 @@
+/* Random site */
 let sites = ["https://zyraclaudius.github.io/theTeapot"];
 let randomSite = document.getElementById('randomsite');
 function linkRandomSite(event){
+    console.log('getting random site');
     let index = Math.floor(Math.random()*sites.length);
     event.target.href = sites[index];
 }
 randomSite.addEventListener('mouseover',linkRandomSite);
+
+/* Secret button */
+let switched = false;
+function switchColors() {
+    console.log('Wow! You found the secret button!');
+    if(switched){
+        switched = false;
+        document.body.style.backgroundColor = 'white';
+        for(let element of document.getElementsByTagName('*')){
+            element.style.color = 'black';
+        }
+        document.querySelector('nav').style.backgroundColor = 'white';
+        let overlays = document.querySelectorAll('.overlay');
+        for(let overlay of overlays){
+            overlay.style.backgroundColor = 'rgba(0,0,0,0.75)';
+            overlay.querySelector('p').style.color = 'white';
+        }
+        let overlay = document.getElementById('overlay');
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.75)';
+        document.querySelector('h1').style.color = 'white';
+        let randomSite = document.getElementById('randomsite');
+        randomSite.style.backgroundColor = 'black';
+        randomSite.style.color = 'white';
+    } else{
+        switched = true;
+        document.body.style.backgroundColor = 'black';
+        for(let element of document.getElementsByTagName('*')){
+            element.style.color = 'white';
+        }
+        document.querySelector('nav').style.backgroundColor = 'black';
+        let overlays = document.querySelectorAll('.overlay');
+        for(let overlay of overlays){
+            overlay.style.backgroundColor = 'rgba(255,255,255,0.75)';
+            overlay.querySelector('p').style.color = 'black';
+        }
+        let overlay = document.getElementById('overlay');
+        overlay.style.backgroundColor = 'rgba(255,255,255,0.75)';
+        document.querySelector('h1').style.color = 'black';
+        let randomSite = document.getElementById('randomsite');
+        randomSite.style.backgroundColor = 'white';
+        randomSite.style.color = 'black';
+    }
+}
+
+let secretButton = document.getElementById('secretbutton');
+secretButton.addEventListener('click',switchColors);
